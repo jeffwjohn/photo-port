852ea188a7c1df899167eca4d3a1689a62d81200
Jeff Johnston <jeffwjohn@yahoo.com>
2020-12-14 11:20:47 -0600

    add form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact"
 
 // Inside the function, it seems that HTML is all that's returned. But actually, it's not HTML; it's a language called JSX that can represent HTML in JavaScript. Normally you can't add HTML to JavaScript, at least without making it a string. But it won't break the code in this case, because you're using webpack and React. Think of functions that return JSX as functions that use document.createElement(JSX). In fact, the way React uses JSX behind the scenes is very similar to document.createElement().
 
@@ -53,10 +54,9 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-          <div>
-            <Gallery currentCategory={currentCategory}></Gallery>
-            <About /> 
-          </div>       
+        <ContactForm></ContactForm>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function ContactForm() {
+    // Hook
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+
+        // We use the spread operator, ...formState, so we can retain the other key-value pairs in this object. Without the spread operator, the formState object would be overwritten to only contain the name: value key pair.
+      }
+      
+    //   console.log(formState);
+     
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+      }
+      
     // JSX
     return (
         <section>
           <h1>Contact me</h1>
-          <form id="contact-form">
+          <form id="contact-form" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name">Name:</label>
-              <input type="text" name="name" />
+              <input type="text" defaultValue={name} onChange={handleChange} name="name" />
             </div>
             <div>
               <label htmlFor="email">Email address:</label>
-              <input type="email" name="email" />
+              <input type="email" defaultValue={email} name="email" onChange={handleChange} />
             </div>
             <div>
               <label htmlFor="message">Message:</label>
-              <textarea name="message" rows="5" />
+              <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
             </div>
             <button type="submit">Submit</button>
           </form>
